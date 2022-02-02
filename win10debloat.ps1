b880a2b3c3a23739055a24a829d277387e386dc2
JacobWilliams22 <98896889+JacobWilliams22@users.noreply.github.com>
2022-02-02 21:00:25 +0000

    Update win10debloat.ps1

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -505,12 +505,12 @@ $discord.height                  = 30
 $discord.location                = New-Object System.Drawing.Point(4,170)
 $discord.Font                    = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
-$WindDirStat                       = New-Object system.Windows.Forms.Button
-$WindDirStat .text                 = "WindDirStat"
-$WindDirStat .width                = 211
-$WindDirStat .height               = 30
-$WindDirStat .location             = New-Object System.Drawing.Point(4,136)
-$WindDirStat .Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
+$WindDirStat                      = New-Object system.Windows.Forms.Button
+$WindDirStat.text                 = "WindDirStat"
+$WindDirStat.width                = 211
+$WindDirStat.height               = 30
+$WindDirStat.location             = New-Object System.Drawing.Point(4,136)
+$WindDirStat.Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
 $appearancefx                    = New-Object system.Windows.Forms.Button
 $appearancefx.text               = "Appearance Visual FX"
