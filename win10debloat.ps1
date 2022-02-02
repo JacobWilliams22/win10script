26370f5dcfba91645a5285f1a06a4b0c380c1654
JacobWilliams22 <98896889+JacobWilliams22@users.noreply.github.com>
2022-02-02 20:56:10 +0000

    Update win10debloat.ps1
    Added WinDirStat

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -505,12 +505,12 @@ $discord.height                  = 30
 $discord.location                = New-Object System.Drawing.Point(4,170)
 $discord.Font                    = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
-$autohotkey                      = New-Object system.Windows.Forms.Button
-$autohotkey.text                 = "AutoHotkey"
-$autohotkey.width                = 211
-$autohotkey.height               = 30
-$autohotkey.location             = New-Object System.Drawing.Point(4,136)
-$autohotkey.Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
+$WindDirStat                       = New-Object system.Windows.Forms.Button
+$WindDirStat .text                 = "WindDirStat "
+$WindDirStat .width                = 211
+$WindDirStat .height               = 30
+$WindDirStat .location             = New-Object System.Drawing.Point(4,136)
+$WindDirStat .Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
 $appearancefx                    = New-Object system.Windows.Forms.Button
 $appearancefx.text               = "Appearance Visual FX"
@@ -627,7 +627,7 @@ $NFS.location                    = New-Object System.Drawing.Point(4,232)
 $NFS.Font                        = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
 $Form.controls.AddRange(@($Panel1,$Panel2,$Label3,$Label15,$Panel4,$PictureBox1,$Label1,$Label4,$Panel3,$ResultText,$Label10,$Label11,$urlfixwinstartup,$urlremovevirus,$urlcreateiso))
-$Panel1.controls.AddRange(@($brave,$firefox,$7zip,$sharex,$adobereader,$notepad,$gchrome,$mpc,$vlc,$powertoys,$ccleaner,$winterminal,$vscode,$Label2,$sumatrapdf,$vscodium,$imageglass,$gimp,$Label7,$Label8,$Label9,$advancedipscanner,$putty,$etcher,$translucenttb,$githubdesktop,$discord,$autohotkey))
+$Panel1.controls.AddRange(@($brave,$firefox,$7zip,$sharex,$adobereader,$notepad,$gchrome,$mpc,$vlc,$powertoys,$ccleaner,$winterminal,$vscode,$Label2,$sumatrapdf,$vscodium,$imageglass,$gimp,$Label7,$Label8,$Label9,$advancedipscanner,$putty,$etcher,$translucenttb,$githubdesktop,$discord,$WindDirStat))
 $Panel2.controls.AddRange(@($essentialtweaks,$backgroundapps,$cortana,$actioncenter,$darkmode,$performancefx,$onedrive,$lightmode,$essentialundo,$EActionCenter,$ECortana,$RBackgroundApps,$HTrayIcons,$EClipboardHistory,$ELocation,$InstallOneDrive,$removebloat,$reinstallbloat,$WarningLabel,$Label5,$appearancefx,$STrayIcons,$EHibernation,$dualboottime))
 $Panel4.controls.AddRange(@($defaultwindowsupdate,$securitywindowsupdate,$Label16,$Label17,$Label18,$Label19))
 $Panel3.controls.AddRange(@($yourphonefix,$Label6,$windowsupdatefix,$ncpa,$oldcontrolpanel,$oldsoundpanel,$Button1,$NFS))
@@ -656,12 +656,12 @@ $gchrome.Add_Click({
     $ResultText.text = "`r`n" + "Finished Installing Google Chrome" + "`r`n" + "`r`n" + "Ready for Next Task"
 })
 
-$autohotkey.Add_Click({
-    Write-Host "Installing AutoHotkey"
-    $ResultText.text = "`r`n" +"`r`n" + "Installing AutoHotkey... Please Wait" 
-    winget install -e Lexikos.AutoHotkey | Out-Host
-    if($?) { Write-Host "Installed AutoHotkey" }
-    $ResultText.text = "`r`n" + "Finished Installing Autohotkey" + "`r`n" + "`r`n" + "Ready for Next Task"
+$WinDirStat.Add_Click({
+    Write-Host "Installing WinDirStat"
+    $ResultText.text = "`r`n" +"`r`n" + "Installing WinDirStat... Please Wait" 
+    winget install -e --id WinDirStat.WinDirStat | Out-Host
+    if($?) { Write-Host "Installed WinDirStat" }
+    $ResultText.text = "`r`n" + "Finished Installing WinDirStat" + "`r`n" + "`r`n" + "Ready for Next Task"
 })
 $imageglass.Add_Click({
     Write-Host "Installing Image Glass (Image Viewer)"
